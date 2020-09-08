73dae36d4a9bb3e48c84379a66375e01bb65a330
Amit998 <damitlucky998@gmail.com>
2020-09-08 22:26:46 +0530

    have model_selection issue

--- movieReview.py
+++ movieReview.py
@@ -30,4 +30,62 @@ for rev in neg_review:
     review_one_string =review_one_string.replace(" \'","'")
     rev_list.append(review_one_string)
 print(len(rev_list))
-pos_review=movie_reviews.fileids('pos')
+# pos_review=movie_reviews.fileids('pos')
+neg_targets=np.zeros((1000,),dtype=np.int)
+pos_targets=np.ones((1000,),dtype=np.int)
+
+target_list=[]
+for neg_target in neg_targets:
+    target_list.append(neg_target)
+for pos_target in pos_targets:
+    target_list.append(pos_target)
+print(len(target_list))
+
+y=pd.Series(target_list)
+print(type(y))
+
+print(y.head())
+
+from sklearn.feature_extraction.text import CountVectorizer
+count_vect=CountVectorizer(lowercase=True,stop_words='english',min_df=2)
+
+x_count_vect=count_vect.fit_transform(rev_list)
+print(x_count_vect.shape)
+
+x_names=count_vect.get_feature_names()
+# print(x_names)
+
+
+x_count_vect=pd.DataFrame(x_count_vect.toarray(),columns=x_names)
+print(x_count_vect.shape)
+
+
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+print(x_count_vect.shape)
+print(y.shape)
+
+
+x_train_cv,x_test_cv,y_train_cv,y_test_cv=train_test_split(x_count_vect,y,test_size=0.25,random_state=5)
+
+
+from sklearn.naive_bayes import GaussianNB
+gnb=GaussianNB()
+y_pred_gnb=gnb.fit(x_train_cv,y_train_cv).predict(x_test_cv)
+
+from sklearn.naive_bayes import MultinomialNB
+clf_cv=MultinomialNB()
+clf_cv.fit(x_train_cv,y_train_cv)
+
+
+y_pred_cv=clf_cv.predict(x_test_cv)
+
+
+print(metrics.accuracy_score(y_test_cv,y_pred_cv))
+
+print(confusion_matrix(y_test_cv,y_pred_cv))
